9559ea6216de2e3039fd032b8930fdbd18156087
Henrik Olai Olsbø Håland <156893021+HenrikHaaland@users.noreply.github.com>
2025-03-17 11:58:01 +0100

    added files and color change for background and text

---
+++ static/htmlgen.js
@@ -0,0 +1,4 @@
+function ColorChange() {    
+    document.body.style.backgroundColor = document.getElementById("bgColor").value;
+    document.body.style.webkitTextFillColor = document.getElementById("textColor").value;
+}
