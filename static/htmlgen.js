d0add688e1c2d0363f9df1ab1dffe007326376b7
Henrik Olai Olsbø Håland <156893021+HenrikHaaland@users.noreply.github.com>
2025-03-18 14:03:40 +0100

    added ability to add h and p tags

--- static/htmlgen.js
+++ static/htmlgen.js
@@ -1,4 +1,29 @@
 function ColorChange() {    
     document.body.style.backgroundColor = document.getElementById("bgColor").value;
-    document.body.style.webkitTextFillColor = document.getElementById("textColor").value;
 }
+
+document.getElementById("addText").addEventListener("click", function(){
+    var p = document.createElement("p");
+    var text = document.createTextNode(document.getElementById("addPTag").value);
+    p.appendChild(text);
+    document.body.appendChild(p);
+
+    p.style.webkitTextFillColor = document.getElementById("textColor").value;
+}
+);
+
+document.getElementById("addHeader").addEventListener("click", function(){
+
+    const size = document.getElementById("hSize").value;
+
+    document.getElementById
+    
+    const text = document.createTextNode(document.getElementById("addHTag").value);
+    const h = document.createElement(size);
+    h.appendChild(text);
+    document.body.appendChild(h);
+
+    h.style.webkitTextFillColor = document.getElementById("headerColor").value;
+}
+);
+
